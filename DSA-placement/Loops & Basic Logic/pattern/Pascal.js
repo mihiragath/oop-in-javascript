ea895719aae5209660a8a6ef09e95fc4fea3a7c0
mihiragath <mihiragath@users.noreply.github.com>
2026-06-16 14:46:12 +0530

    add

--- DSA-placement/Loops & Basic Logic/pattern/Pascal.js
+++ DSA-placement/Loops & Basic Logic/pattern/Pascal.js
@@ -12,3 +12,20 @@ for (let i = 0; i < n; i++) {
 
   console.log(row);
 }
+
+//another way
+
+let num = 5;
+let result = [];
+
+for (let i = 0; i < num; i++) {
+  let row = new Array(i + 1).fill(1);
+
+  for (let j = 1; j < i; j++) {
+    row[j] = result[i - 1][j - 1] + result[i - 1][j];
+  }
+
+  result.push(row);
+}
+
+console.log(result);
